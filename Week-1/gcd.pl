b3a2aa88093217e46a35e6117272ede79652e441
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-07-26 14:44:01 +0530

    updated gcd code

--- Week-1/gcd.pl
+++ Week-1/gcd.pl
@@ -1,4 +1,7 @@
-gcd(X, Y, GCD) :- X=0, GCD=Y.
-gcd(X, Y, GCD) :- Y=0, GCD=X.
-gcd(X, Y, GCD) :- X>Y, gcd(Y, X, GCD).
-gcd(X, Y, GCD) :- X<Y, M is Y mod X, gcd(X, M, GCD).
+% gcd(X, Y, GCD) :- X=0, GCD=Y.
+% gcd(X, Y, GCD) :- Y=0, GCD=X.
+% gcd(X, Y, GCD) :- X>Y, gcd(Y, X, GCD).
+% gcd(X, Y, GCD) :- X<Y, M is Y mod X, gcd(X, M, GCD).
+
+gcd(X,0,X) :- X > 0.
+gcd(X,Y,G) :- Y > 0, Z is X mod Y, gcd(Y,Z,G).
